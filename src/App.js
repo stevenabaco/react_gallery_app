3bf2cf512896993a510e35191e51de501a8577d6
Steven Abaco <stevenabaco@gmail.com>
2021-05-26 09:53:36 -0500

    Add logic to check state of Loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 // Import packages
 import React, { Component } from 'react';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import axios from 'axios';
 
 // Import Config and CSS files
@@ -20,44 +16,48 @@ class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-      nature: [],
-      food: [],
-      mountains: [],
-      photos: [],
-      loading: true,
-      tag: null
+			nature: [],
+			food: [],
+			mountains: [],
+			photos: [],
+			loading: true, // Add state loading indicator for exceeds
+			tag: null,
 		};
 	}
-
+	// Call function to search for default pictures
 	componentDidMount() {
-    this.performSearch('nature');
-    this.performSearch('food');
-    this.performSearch('mountains');
+		this.performSearch('nature');
+		this.performSearch('food');
+		this.performSearch('mountains');
 	}
-  performSearch = (query) => {
+	// Create function with logic to query Flickr API
+	performSearch = query => {
 		axios
+			// Send GET request to API to fetch data
 			.get(
 				`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=%22"${query}"%22&per_page=24&format=json&nojsoncallback=1`
 			)
 			.then(response => {
-				// handle success
-        if (query === 'nature') {
-          this.setState({
-            nature: response.data.photos.photo,
-            tag: query
-          })
-        } else if (query === 'food') {
-          this.setState({
-            food: response.data.photos.photo,
-            tag: query
-          })
-        } else if (query === 'mountains') {
-          this.setState(({
-            mountains: response.data.photos.photo,
-            tag: query
-          }))
-        }
-      
+				// handle success conditionally to update state properly
+				if (query === 'nature') {
+					this.setState({
+						nature: response.data.photos.photo,
+						tag: query,
+						loading: false,
+					});
+				} else if (query === 'food') {
+					this.setState({
+						food: response.data.photos.photo,
+						tag: query,
+						loading: false,
+					});
+				} else if (query === 'mountains') {
+					this.setState({
+						mountains: response.data.photos.photo,
+						tag: query,
+						loading: false,
+					});
+				}
 			})
 			.catch(error => {
 				// handle error
@@ -69,7 +69,6 @@ class App extends Component {
 	};
 
 	render() {
-	
 		return (
 			<Router>
 				<div className='container'>
@@ -78,13 +77,28 @@ class App extends Component {
 				</div>
 				<Switch>
 					<Route path='/nature'>
-						<Gallery data={this.state.nature} title='Nature' />
+						<Gallery
+							data={this.state.nature}
+							title='Nature'
+							isLoading={this.state.isLoading}
+						/>
 					</Route>
 					<Route path='/food'>
-						<Gallery data={this.state.food} title='Food'/>
+						<Gallery
+							data={this.state.food}
+							title='Food'
+							isLoading={this.state.isLoading}
+						/>
 					</Route>
 					<Route path='/mountains'>
-						<Gallery data={this.state.mountains} title='Mountains' />
+						<Gallery
+							data={this.state.mountains}
+							title='Mountains'
+							isLoading={this.state.isLoading}
+						/>
+					</Route>
+					<Route path='/search/:query'>
+						<Gallery />
 					</Route>
 				</Switch>
 			</Router>
